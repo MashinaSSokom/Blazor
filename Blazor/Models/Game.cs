67ca67e1076786757f7dd55db5a35d890346eac1
MashinaSSokom <skltan@mail.ru>
2021-05-01 15:54:18 +0800

    Добавьте файлы проекта.

---
+++ Blazor/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
